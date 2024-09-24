87dc03bc778a9c77627be18b0429f2717e7d2c24
NUS17461-11-rohtumm <rohtumm@nuevaschool.org>
2024-09-24 13:44:28 -0700

    updated homepage

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bienvenido a tu Rastreador de Hábitos</h1>
+        <p>Please log in to track your Spanish habits.</p>
+      </header>
+    </div>
+  );
+}
+
+
+
+export default App;
